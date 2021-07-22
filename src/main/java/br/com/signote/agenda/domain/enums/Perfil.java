6f7b061312b74c3ce7ab8d3fd00296e6b4965a02
Miguel Arcanjo <mig-ramos10@gmail.com>
2021-07-22 10:18:42 -0300

    Criado o enum Perfil com os tipos exigidos pelo Spring Boot

---
+++ src/main/java/br/com/signote/agenda/domain/enums/Perfil.java
@@ -0,0 +1,39 @@
+package br.com.signote.agenda.domain.enums;
+
+public enum Perfil {
+	
+	ADMIN(1, "ROLE_ADMIN"), // ROLER_ é exigencia do Spring Security
+	PACIENTE(2, "ROLE_PACIENTE"),
+	MEDICO(3, "ROLE_MEDICO");
+	
+	private int cod;
+	private String descricao;
+	
+	private Perfil(int cod, String descricao) {
+		this.cod = cod;
+		this.descricao = descricao;
+	}
+	
+	public int getCod() {
+		return cod;
+	}
+	
+	public String getDescricao () {
+		return descricao;
+	}
+	
+	public static Perfil toEnum(Integer cod) {
+		
+		if (cod == null) {
+			return null;
+		}
+		
+		for (Perfil x : Perfil.values()) {
+			if (cod.equals(x.getCod())) {
+				return x;
+			}
+		}
+		
+		throw new IllegalArgumentException("Id inválido: " + cod);
+	}
+}
