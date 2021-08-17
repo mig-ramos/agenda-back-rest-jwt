5941f92ee88d0080d280852b2d1a022260a7e2ec
Miguel Arcanjo <mig.ramos10@gmail.com>
2021-08-17 17:38:30 -0300

    Acerta configuracao para producao - 1

--- src/main/java/br/com/signote/agenda/config/ProdConfig.java
+++ src/main/java/br/com/signote/agenda/config/ProdConfig.java
@@ -1,14 +1,9 @@
 package br.com.signote.agenda.config;
 
-import java.text.ParseException;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import br.com.signote.agenda.services.DBService;
 import br.com.signote.agenda.services.EmailService;
 import br.com.signote.agenda.services.SmtpEmailService;
 
@@ -16,22 +11,22 @@ import br.com.signote.agenda.services.SmtpEmailService;
 @Profile("prod")
 public class ProdConfig {
 
-	@Autowired
-	private DBService dbService;
-	
-	@Value("${spring.jpa.hibernate.ddl-auto}")
-	private String strategy;
-	
-	@Bean
-	public boolean instantiateDatabase() throws ParseException {
-		
-		if (!"create".equals(strategy)) {
-			return true; // Não recria o banco de dados
-		}
-		
-		dbService.instantiateTestDatabase();
-		return true;
-	}
+//	@Autowired
+//	private DBService dbService;
+//	
+//	@Value("${spring.jpa.hibernate.ddl-auto}")
+//	private String strategy;
+//	
+//	@Bean
+//	public boolean instantiateDatabase() throws ParseException {
+//		
+//		if (!"create".equals(strategy)) {
+//			return true; // Não recria o banco de dados
+//		}
+//		
+//		dbService.instantiateTestDatabase();
+//		return true;
+//	}
 	
 	@Bean
 	public EmailService emailService() {
