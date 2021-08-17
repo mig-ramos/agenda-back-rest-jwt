052c7b7dd2b0666fe233200f7d24221098eddc3a
Miguel Arcanjo <mig.ramos10@gmail.com>
2021-08-16 22:07:47 -0300

    Documentacao com swagger - parte 1

--- src/main/java/br/com/signote/agenda/config/SecurityConfig.java
+++ src/main/java/br/com/signote/agenda/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -25,34 +26,30 @@ import br.com.signote.agenda.security.JWTUtil;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true) // Vai permitir colocar anotacao preautorizacoes nos endpoints 
+@EnableGlobalMethodSecurity(prePostEnabled = true) // Vai permitir colocar anotacao preautorizacoes nos endpoints
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	@Autowired
 	private UserDetailsService userDetailsService;
 
 	@Autowired
 	private Environment env;
-	
+
 	@Autowired
 	private JWTUtil jwtUtil;
 
-	private static final String[] PUBLIC_MATCHERS = { 
-			"/h2-console/**" 
-			};
+	private static final String[] PUBLIC_MATCHERS = { "/h2-console/**" };
 
-	private static final String[] PUBLIC_MATCHERS_GET = { 
-			"/pacientes/**",
-			"/usuarios/**",
-			"/medicos/**"			
-			};
-	
-	private static final String[] PUBLIC_MATCHERS_POST = { 
-			"/pacientes/**",
-			"/usuarios/**",
-			"/medicos/**",
-			"/auth/forgot/**"
-			};
+	private static final String[] PUBLIC_MATCHERS_GET = { "/pacientes/**", "/usuarios/**", "/medicos/**" };
+
+	private static final String[] PUBLIC_MATCHERS_POST = { "/pacientes/**", "/usuarios/**", "/medicos/**",
+			"/auth/forgot/**" };
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**", "/configuration/**",
+				"/swagger-ui.html", "/webjars/**");
+	}
 
 	// Subescrever o método
 	@Override
@@ -63,16 +60,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		}
 
 		http.cors().and().csrf().disable();
-		http.authorizeRequests()
-		.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
-		.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll() // So vai permitir o metodo GET, quem tiver na lista
-		.antMatchers(PUBLIC_MATCHERS).permitAll()
-		.anyRequest().authenticated();
+		http.authorizeRequests().antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
+				.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll() // So vai permitir o metodo GET, quem
+																				// tiver na lista
+				.antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
 		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
-	
+
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		// Quem vai ser capaz de buscar o usuário por email
@@ -85,7 +81,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
 		return source;
 	}
-	
+
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
