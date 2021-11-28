8f88ce871852e69268c2ede2d9987f6620aee5e7
Miguel Arcanjo <mig.ramos10@gmail.com>
2021-11-27 23:20:22 -0300

    Limpando as credenciais

--- src/main/java/br/com/signote/agenda/services/DBService.java
+++ src/main/java/br/com/signote/agenda/services/DBService.java
@@ -54,15 +54,16 @@ SimpleDateFormat sdf1= new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		
 		Especialidade esp1 = new Especialidade(null,"Pediatra", "Descrição do Pediatra");
 		Especialidade esp2 = new Especialidade(null,"Clínico Geral", "Descrição do Clínico Geral");
+		Especialidade esp3 = new Especialidade(null,"Nutrólogo", "Descrição do Nutrólogo");	
 		
 		Usuario usu1 = new Usuario(null,"Administrador", "adm.ephemeris@gmail.com", pe.encode("123"),"123456", sdf1.parse("14/05/2021 09:45"), true);
 		usu1.addPerfil(Perfil.ADMIN);
 		
-		Paciente pac1 = new Paciente(null, "Paciente da Silva","p.ephemeris@gmail.com", pe.encode("123"),"123456",sdf1.parse("10/08/2021 13:40"), true, sdf2.parse("14/05/1959")) ;
-		
-		Medico med1 = new Medico(null, "Médico da Silva", "m.ephemeris@gmail.com", pe.encode("123"),"123456",sdf1.parse("04/05/2021 10:45"), true, 123456, sdf2.parse("14/05/2008"));
-		
-		Medico med2 = new Medico(null, "Médico Siderbit", "siderbit@gmail.com", pe.encode("123"),"789123",sdf1.parse("14/10/2021 12:45"), true, 435465, sdf2.parse("14/05/2010"));
+//		Paciente pac1 = new Paciente(null, "Simpson da Silva","p.ephemeris@gmail.com", pe.encode("123"),"123456",sdf1.parse("10/08/2021 13:40"), true, sdf2.parse("14/05/1959")) ;
+//		
+//		Medico med1 = new Medico(null, "Hipócrates Grego", "m.ephemeris@gmail.com", pe.encode("123"),"123456",sdf1.parse("04/05/2021 10:45"), true, 123456, sdf2.parse("14/05/2008"));
+//		
+//		Medico med2 = new Medico(null, "Lair Ribeiro", "siderbit@gmail.com", pe.encode("123"),"789123",sdf1.parse("14/10/2021 12:45"), true, 435465, sdf2.parse("14/05/2010"));
 		
 		Hora hr1 = new Hora(null, "8:00");
 		Hora hr2 = new Hora(null, "9:00");
@@ -73,32 +74,36 @@ SimpleDateFormat sdf1= new SimpleDateFormat("dd/MM/yyyy HH:mm");
 		Hora hr7 = new Hora(null, "15:00");
 		Hora hr8 = new Hora(null, "16:00");
 		
-		Especialidade esp3 = new Especialidade(null,"Otorrino", "Descrição do Otorrino");
-		Especialidade esp4 = new Especialidade(null,"Geriatra", "Descrição do Geriatra");
-		Especialidade esp5 = new Especialidade(null,"Dermatologista", "Descrição do Dermatologista");
-		Especialidade esp6 = new Especialidade(null,"Gastro", "Descrição do Gastro");
-		Especialidade esp7 = new Especialidade(null,"Psiquiatra", "Descrição do Psiquiatra");			
+//		Especialidade esp4 = new Especialidade(null,"Otorrino", "Descrição do Otorrino");
+//		Especialidade esp5 = new Especialidade(null,"Geriatra", "Descrição do Geriatra");
+//		Especialidade esp6 = new Especialidade(null,"Dermatologista", "Descrição do Dermatologista");
+//		Especialidade esp7 = new Especialidade(null,"Gastro", "Descrição do Gastro");
+			
 		
 		usuarioRepository.saveAll(Arrays.asList(usu1));		
-		pacienteRepository.save(pac1);
+	//	pacienteRepository.save(pac1);
 		
-		esp1.getMedicos().addAll(Arrays.asList(med1));
-		esp2.getMedicos().addAll(Arrays.asList(med1));
-		med1.getEspecialidades().addAll(Arrays.asList(esp1, esp2));			
+//		esp1.getMedicos().addAll(Arrays.asList(med1));
+//		esp2.getMedicos().addAll(Arrays.asList(med1));
+//		esp3.getMedicos().addAll(Arrays.asList(med2));
+//		med1.getEspecialidades().addAll(Arrays.asList(esp1, esp2));	
+//		med2.getEspecialidades().addAll(Arrays.asList(esp3));
 		
-		medicoRepository.saveAll(Arrays.asList(med1,med2));		
-		especialidadeRepository.saveAll(Arrays.asList(esp1, esp2, esp3, esp4, esp5, esp6, esp7));
+	//	medicoRepository.saveAll(Arrays.asList(med1,med2));		
+//		especialidadeRepository.saveAll(Arrays.asList(esp1, esp2, esp3, esp4, esp5, esp6, esp7));
+//		especialidadeRepository.saveAll(Arrays.asList(esp1, esp2, esp3));
 		horaRepository.saveAll(Arrays.asList(hr1, hr2, hr3, hr4, hr5, hr6, hr7, hr8));
 		
-		TipoConsulta tpCon1 = new TipoConsulta(null, "Primeira Consulta");
-		TipoConsulta tpCon2 = new TipoConsulta(null, "Retorno Consulta");
+		TipoConsulta tpCon1 = new TipoConsulta(null, "Consulta");
+		TipoConsulta tpCon2 = new TipoConsulta(null, "Exames");
+		TipoConsulta tpCon3 = new TipoConsulta(null, "Retorno");
 	
-		tipoConsultaRepository.saveAll(Arrays.asList(tpCon1, tpCon2));
+		tipoConsultaRepository.saveAll(Arrays.asList(tpCon1, tpCon2, tpCon3));
 		
-		Agenda age1 = new Agenda(null, esp2, med1, LocalDate.of(2021, 07, 24), hr1, tpCon1, pac1, null, null, null);
-		Agenda age2 = new Agenda(null, esp1, med1, LocalDate.of(2021, 07, 25), hr2, tpCon2, pac1, null, "Paciente sem anomalia", null);
-		
-		agendaRepository.saveAll(Arrays.asList(age1,age2));
+//		Agenda age1 = new Agenda(null, esp2, med1, LocalDate.of(2021, 07, 24), hr1, tpCon1, pac1, null, "", null);
+//		Agenda age2 = new Agenda(null, esp1, med1, LocalDate.of(2021, 07, 25), hr2, tpCon2, pac1, null, "", null);
+//		
+//		agendaRepository.saveAll(Arrays.asList(age1,age2));
 	}
 
 }
